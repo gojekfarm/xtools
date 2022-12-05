feac002e4bea1a3fee1db1cdf17e9bdd3da96e9f
Ajat Prabha <ajatprabha@users.noreply.github.com>
2022-12-05 14:42:32 +0530

    add xproto (#1)

---
+++ xproto/doc.go
@@ -0,0 +1,2 @@
+// Package xproto contains Encoder and Decoder for working with google.golang.org/protobuf types.
+package xproto // import "github.com/gojekfarm/xtools/xproto"
---
+++ xproto/go.mod
@@ -0,0 +1,5 @@
+module github.com/gojekfarm/xtools/xproto
+
+go 1.16
+
+require google.golang.org/protobuf v1.27.1
