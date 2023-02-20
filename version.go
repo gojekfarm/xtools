90fbe4ad46f60c0a4b313ecf8719b55b5a1952ec
ajatprabha <ajat.prabha.leo@gmail.com>
2023-02-20 13:25:00 +0530

    Releasing v0.1.1

--- version.go
+++ version.go
@@ -2,6 +2,6 @@ package xtools
 
 // Version can be used to get the current xtools library version
 func Version() string {
-	return "0.1.0"
+	return "0.1.1"
 	// This string is updated by the pre_release.sh script during release
 }
