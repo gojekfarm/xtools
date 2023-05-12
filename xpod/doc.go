dfa954b396f7e4147db9b307b347b1b5f08d4783
ajatprabha <ajat.prabha.leo@gmail.com>
2023-05-12 18:15:23 +0530

    update docs

--- xpod/doc.go
+++ xpod/doc.go
@@ -1,3 +1,5 @@
 // Package xpod contains utilities that help implement best practices for health checks and more,
 // while building go apps for kubernetes pods.
+//
+// Health checker source comes from k8s.io/apiserver/pkg/server/healthz/healthz.go
 package xpod
