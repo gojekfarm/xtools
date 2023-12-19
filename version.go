00201458264fb17aa4ff1a22d446ccf1f9c42283
Ravi Atluri <ravi.atluri@gojek.com>
2023-12-19 10:27:09 +0530

    Releasing v0.5.0 (#29)
    * Releasing v0.5.0
    
    * Updated Changelog

--- version.go
+++ version.go
@@ -2,6 +2,6 @@ package xtools
 
 // Version can be used to get the current xtools library version
 func Version() string {
-	return "0.4.1"
+	return "0.5.0"
 	// This string is updated by the pre_release.sh script during release
 }
--- xload/providers/yaml/go.mod
+++ xload/providers/yaml/go.mod
@@ -5,7 +5,7 @@ go 1.20
 replace github.com/gojekfarm/xtools/xload => ../..
 
 require (
-	github.com/gojekfarm/xtools/xload v0.4.1
+	github.com/gojekfarm/xtools/xload v0.5.0
 	gopkg.in/yaml.v3 v3.0.1
 )
 
