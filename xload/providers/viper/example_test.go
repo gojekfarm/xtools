7943c42d9c376506c685dcf73ad5dd54052e135c
ajatprabha <ajat.prabha.leo@gmail.com>
2024-01-11 09:44:38 +0530

    add examples

--- xload/providers/viper/example_test.go
+++ xload/providers/viper/example_test.go
@@ -37,3 +37,55 @@ func ExampleNew() {
 	// Output:
 	// debug
 }
+
+func ExampleFrom() {
+	_, err := viper.New(viper.From(vpr.New()))
+	if err != nil {
+		panic(err)
+	}
+}
+
+func ExampleLoader_ConfigFileUsed() {
+	_, err := viper.New(viper.ConfigFile("<path-to-config-file>.ext"))
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(vpr.ConfigFileUsed()) // <path-to-config-file>.ext
+}
+
+func ExampleNew_fileOptions() {
+	_, err := viper.New(
+		viper.ConfigName("config"),
+		viper.ConfigType("toml"),
+		viper.ConfigPaths([]string{"./", "/etc/<program/"}),
+	)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func ExampleAutoEnv_disable() {
+	_, err := viper.New(viper.AutoEnv(false))
+	if err != nil {
+		panic(err)
+	}
+}
+
+func ExampleValueMapper() {
+	_, err := viper.New(viper.ValueMapper(func(m map[string]any) map[string]string {
+		return xload.FlattenMap(m, "__")
+	}))
+	if err != nil {
+		panic(err)
+	}
+}
+
+func ExampleTransformer() {
+	_, err := viper.New(viper.Transformer(func(v *vpr.Viper, next xload.Loader) xload.Loader {
+		return xload.PrefixLoader("ENV_", next)
+	}))
+	if err != nil {
+		panic(err)
+	}
+}
